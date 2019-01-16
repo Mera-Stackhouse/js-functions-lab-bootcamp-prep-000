80b537dd6f16a697372fb848d308ec90425881de
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-01-16 03:49:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+
+
+function happyHolidays() {
+  return "Happy Holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return `Happy Holidays, ${name}!`
+}
+
+function HappyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(days, holiday) {
+  return `It's ${days} days until ${holiday}!`
+}
+
